6cf6ebe98ffce927342b6005c1e0a467a6a8fa18
dan <haggard.dan@gmail.com>
2018-02-21 14:38:26 -0700

    updating installs.sh to actually do everything

--- installs.sh
+++ installs.sh
@@ -1,10 +1,8 @@
-
-
-
-
 # rxvt-unicode
 
-sudo apt install rxvt-unicode-256color fonts-inconsolata
+sudo apt install -y rxvt-unicode-256color fonts-inconsolata
+cat Xresources >> ~/.Xdefaults
+mkdir -d ~/.urxvt/ext
 cp vtwheel ~/.urxvt/ext/vtwheel
 # exec --no-startup-id urxvtd -q -f -o
 
@@ -12,37 +10,38 @@ cp vtwheel ~/.urxvt/ext/vtwheel
 # python
 
 sudo apt install python-pip direnv
-sudo pip install --upgrade pip
-sudo pip install virtualenv
+sudo pip install --upgrade pip virtualenv
 echo "layout python" > ~/.envrc
-# export DIRENV_LOG_FORMAT=""
-# eval "$(direnv hook bash) &> /dev/null"
+echo "export DIRENV_LOG_FORMAT=\"\"" >> ~/.bashrc
+echo "eval \"\$(direnv hook bash)\"" >> ~/.bashrc
+direnv allow ~/.
 
 # vim
 
-sudo apt install vim-gtk
+sudo apt install -y vim-gtk
+cp vimrc ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
 # atom
 
-sudo add-apt-repository ppa:webupd8team/atom
+sudo add-apt-repository -y ppa:webupd8team/atom
 sudo apt update
 sudo apt install atom
 apm install minimap atom-material-ui atom-material-syntax go-plus git-blame linter-pylama teletype vim-mode-plus editorconfig
+cp atom_config.cson ~/.atom/config.cson
 
 # backlight
 
 git clone https://github.com/haikarainen/light.git
 sudo apt install -y help2man
 cd light
-make && make install
-# bindsym XF86MonBrightnessUp exec light -A 5 # increase screen brightness
-# bindsym XF86MonBrightnessDown exec light -U 5 # decrease screen brightness
-#
-# bindsym F10 exec --no-startup-id pactl set-sink-volume 1 +5% #increase sound volume
-# bindsym F9 exec --no-startup-id pactl set-sink-volume 1 -5% #decrease sound volume
-# bindsym F8 exec --no-startup-id pactl set-sink-mute 1 toggle # mute sound
+make && sudo make install
+cd ..
 
-# bindsym $mod+m bar mode toggle
-# new_window pixel 1
+# system
+sudo apt install -y feh xautolock
+# this needs to be templated
+sudo cp i3lock.service /etc/systemd/system/.
+sudo systemctl enable i3lock.service
+
