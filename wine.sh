c263336549fd3aec3530cacc12a42e2e4fc3c4b8
dan <haggard.dan@gmail.com>
2018-03-08 10:12:01 -0700

    adding lutris to the wine install

--- wine.sh
+++ wine.sh
@@ -1,6 +1,11 @@
+ver=$(lsb_release -sr); if [ $ver != "17.10" -a $ver != "17.04" -a $ver != "16.04" ]; then ver=16.04; fi
+echo "deb http://download.opensuse.org/repositories/home:/strycore/xUbuntu_$ver/ ./" | sudo tee /etc/apt/sources.list.d/lutris.list
+wget -q http://download.opensuse.org/repositories/home:/strycore/xUbuntu_$ver/Release.key -O- | sudo apt-key add -
+
+
 wget -nc https://repos.wine-staging.com/wine/Release.key
 sudo apt-key add Release.key
 sudo apt-add-repository 'https://dl.winehq.org/wine-builds/ubuntu/'
 sudo apt-get update
-sudo apt-get install --install-recommends winehq-staging
+sudo apt-get install --install-recommends winehq-staging lutris
 
