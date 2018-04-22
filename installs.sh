314e267f1e4e165b473ce4fa94bdfee6e659dbd2
dan <haggard.dan@gmail.com>
2018-04-21 22:37:26 -0600

    adding spotify to installs.sh

--- installs.sh
+++ installs.sh
@@ -59,3 +59,9 @@ sudo apt install -y udiskie
 # VPN
 sudo apt install -y network-manager-openconnect-gnome
 
+# spotify
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt update
+sudo apt -y install spotify-client
+
