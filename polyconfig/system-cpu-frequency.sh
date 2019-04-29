3b37c2af4c95d7473dc41b252fb9e0bdeb72ef9c
dan <haggard.dan@gmail.com>
2019-04-29 09:48:15 -0600

    adding some changes to polybar

--- polyconfig/system-cpu-frequency.sh
+++ polyconfig/system-cpu-frequency.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-cat /proc/cpuinfo | grep "^[c]pu MHz" | awk '{s+=$4} END {printf "%.0f\n", s / 48}'
+#cat /proc/cpuinfo | grep "^[c]pu MHz" | awk '{s+=$4} END {printf "%.0f\n", s / 48}'
+cat /proc/cpuinfo | grep "^[c]pu MHz" | awk '{s+=$4; ssq += ($4)^2; ++n} END {printf "%4.0f MHz σ%-3.0f\n", s / 48, sqrt((1/n)*(ssq-((s^2)/n)))}'
